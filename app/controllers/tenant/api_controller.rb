3ac61b31ec116cb6b620859b4f57467c0e71c640
Mateusz Zdanowicz <mateusz.zdanowicz@elpassion.pl>
2016-09-22 14:50:17 +0200

    Create calendar of reservations endpoint

--- app/controllers/tenant/api_controller.rb
+++ app/controllers/tenant/api_controller.rb
@@ -2,5 +2,9 @@ module Tenant
   class ApiController < ActionController::Base
     include CustomerConcern
     protect_from_forgery with: :null_session
+
+    def current_user
+      @current_user ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+    end
   end
 end
