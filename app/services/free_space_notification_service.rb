060cb6b2a42f3b7d645201253a7deea747201ccf
Mateusz Zdanowicz <mateusz.zdanowicz@elpassion.pl>
2016-09-23 15:06:41 +0200

    Send free space notifications to interested users

--- app/services/free_space_notification_service.rb
+++ app/services/free_space_notification_service.rb
@@ -1,6 +1,7 @@
 class FreeSpaceNotificationService
   def execute(user, date)
-    user_ids = User.where.not(id: Reservation.where(reservation_date: date).pluck(:user_id) << user.id).pluck(:id)
+    user_ids = User.where(free_space_notifications: true)
+                   .where.not(id: Reservation.where(reservation_date: date).pluck(:user_id) << user.id).pluck(:id)
     FreeSpaceNotificationJob.perform_later(user_ids, Apartment::Tenant.current)
   end
 end
