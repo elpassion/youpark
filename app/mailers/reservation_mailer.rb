bd8768150c86ba0193a82d2b235edc13d7f99b44
aserafin <adrian@softmad.pl>
2016-09-23 13:59:47 +0200

    change confirm reservation email

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -3,7 +3,7 @@ class ReservationMailer < ApplicationMailer
   def confirm(user_id, tenant)
     Apartment::Tenant.switch(tenant) do
       @user = User.find(user_id)
-      mail(to: @user.email, subject: 'YouPark - potwierdzenie rezerwacji')
+      mail(to: @user.email, subject: '[YouPark] Będziesz korzystał ze swojego miejsca parkingowego?')
     end
   end
 end
