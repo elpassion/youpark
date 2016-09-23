f93c726224d1a553a49d24b272b5d9247dde7a0f
aserafin <adrian@softmad.pl>
2016-09-23 14:43:58 +0200

    fix reservations controller

--- app/services/create_reservation_service.rb
+++ app/services/create_reservation_service.rb
@@ -2,7 +2,7 @@ class CreateReservationService
   def execute(user, date, parking_space_id = nil)
     Reservation.transaction do
       reservations = Reservation.free.on_date(date).order(id: :asc)
-      reservations = reservations.where(parking_space_id: parking_space_id) if parking_space_id.nil?
+      reservations = reservations.where(parking_space_id: parking_space_id) unless parking_space_id.nil?
 
       reservations.lock(true).first.tap do |reservation|
         reservation.update_columns(user_id: user.id) unless reservation.nil?
