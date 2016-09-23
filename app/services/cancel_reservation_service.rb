9b99b9ba16fb0287d6a4ae20494749338f11a610
aserafin <adrian@softmad.pl>
2016-09-23 15:03:08 +0200

    check affected rows before sending email

--- app/services/cancel_reservation_service.rb
+++ app/services/cancel_reservation_service.rb
@@ -1,6 +1,6 @@
 class CancelReservationService
   def execute(user, date)
-    Reservation.on_date(date).for_user(user).update_all(user_id: nil, reservation_rule_id: nil)
-    FreeSpaceNotificationService.new.execute(user, date)
+    affected_rows = Reservation.on_date(date).for_user(user).update_all(user_id: nil, reservation_rule_id: nil)
+    FreeSpaceNotificationService.new.execute(user, date) if affected_rows > 0
   end
 end
