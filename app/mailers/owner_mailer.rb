d33dfccf872a810fccb98fa9ad5b9a0db740ab48
aserafin <adrian@softmad.pl>
2016-09-23 12:46:07 +0200

    change to deliver now for emails

--- app/mailers/owner_mailer.rb
+++ app/mailers/owner_mailer.rb
@@ -2,12 +2,10 @@ class OwnerMailer < ApplicationMailer
   default from: 'system@youpark.herokuapp.com'
   layout 'mailer'
 
-  def make_reservation_email(owner, tenant)
-    Apartment::Tenant.switch(tenant) do
-      @user = owner.user
-      @parking_space = owner.parking_space
+  def make_reservation_email(owner)
+    @user = owner.user
+    @parking_space = owner.parking_space
 
-      mail(to: @user.email, subject: '[ElParking] Stwórz swoją rezerwację!')
-    end
+    mail(to: @user.email, subject: '[ElParking] Stwórz swoją rezerwację!')
   end
 end
--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -1,9 +1,7 @@
 class ReservationMailer < ApplicationMailer
 
-  def confirm(user, tenant)
-    Apartment::Tenant.switch(tenant) do
-      @user = user
-      mail(to: @user.email, subject: 'YouPark - potwierdzenie rezerwacji')
-    end
+  def confirm(user)
+    @user = user
+    mail(to: @user.email, subject: 'YouPark - potwierdzenie rezerwacji')
   end
 end
